9a3f7c7c2b67b4c36b703e24be51d2e53dec1e12
Galdin Raphael <gldraphael@users.noreply.github.com>
2023-08-09 17:06:39 +0530

    Initial commit
    See: https://github.com/gldraphael/mt-req-response-exp

---
+++ ClientApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using CommonLib.Extensions;
+using CommonLib.MessageContracts;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System.IO;
+
+
+// setup configuration
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .Build();
+
+// configure serilog
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(config)
+    .CreateLogger();
+
+// setup the DI container
+var serviceProvider = new ServiceCollection()
+    .AddLogging(lb => lb.AddSerilog(dispose: true))
+    .AddMassTransit(
+            config: config,
+            configureDependencies: x =>
+            {
+                x.AddRequestClient<GetInfo>();
+            },
+            configureEndpoints: (bc, rc, sp) => {  },
+            configureRabbitMq: (rbc, sp) => { },
+            configureAzureServiceBus: (sbc, sp) => { }
+        )
+    .BuildServiceProvider();
+    
+
+
+var bus = serviceProvider.GetRequiredService<IBusControl>();
+await bus.StartAsync();
+
+try
+{
+    
+}
+finally
+{
+    await bus.StopAsync();
+}
---
+++ CommonLib/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using CommonLib.Internal;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace CommonLib.Extensions
+{
+    internal static class ConfigurationExtensions
+    {
+        public static string GetAzureServiceBusConnectionString(this IConfiguration config) =>
+            config.GetConnectionString("AzureServiceBus") ?? throw new InvalidOperationException();
+
+        public static MessagingTransport GetMessagingTransport(this IConfiguration config) =>
+            config.GetValue<MessagingTransport>("Messaging:Transport");
+
+        public static RabbitMQOptions GetRabbitMqOptions(this IConfiguration config) =>
+            config.GetSection("RabbitMQ").Get<RabbitMQOptions>() ?? throw new InvalidOperationException();
+    }
+}
